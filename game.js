5b9b76f7b4ba611f7118f9293f6a16c308aeaba5
ereism1 <eduardomreis2@gmail.com>
2024-04-14 20:11:01 -0300

    inicial

--- game.js
+++ game.js
@@ -1,68 +1,42 @@
-const canvas = document.GetElemntById("game-canvas");
-let PlatformGap = 0;
-let score = 0;
-let highscore = 0;
- 
-class Doodler{
-constructor(){
-this.context = canvas.getcontext("2d")
-this.x = canvas.width / 2;
-this.y = canvas.height - 200;
-this.image = new Image ();
-this.image.src = "colocar endereço"
-this.prevY = this.y;
-this.width = 50;
-this.height = 50;
-this.vx = 0;
-this.vy = 0;
-this.gravity = 0.03;
-this.jumpStrength = -2.5;
- }
-//função que muda a posição do doodler
-updatePosition(){
-this.prevY = this.y;
-this.x += this.vx;
-this.y += this.vy;
-if(this.vy > 3.5){
-this.vy = 3.5;
-}else{
-this.vy += this.gravity;
+const dino = document.getElementById("dino")
+const pedra = document.getElementById("pedra")
+const score = document.getElementById("score")
+const botao = document.getElementById("botao")
+
+let jogoRodando = false;
+let intervalo;
+
+function pular() {
+    dino.classList.add('pular-animation');
+    setTimeout( () => {
+        dino.classList.remove('pular-animation');
+    }, 500);
 }
-this.checkForWrapDoodler();
-// função que checa se
-// o doodler está fora do canvas horizontalmente
-// f.e doodler deixa o canvas para a esquerda
-// ele vai entrar novamente pela direita
-// e vice-versa
-checkForWrapDoodler()
+document.addEventListener('keypress', () => {
+    if (!dino.classList.contains("pular-animation")){
+        pular();
+    }
+    
+})
+
+function comecar(){
+        if (!jogoRodando) {
+            jogoRodando = true;}
+        intervalo = setInterval( () => {
+    score.innerHTML++;
+    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top") );
+    const pedraLeft = parseInt(window.getComputedStyle(pedra).getPropertyValue("left"));
+    if (pedraLeft < 0) {
+        pedra.style.display = 'none';
+    }else {
+        pedra.style.display = '';
+    }
+
+    if(pedraLeft < 40 && pedraLeft > 0 && dinoTop > 250) {
+     alert('parabéns,seu bosta');
+     location.reload();
+    }
+        
+}, 50)
 }
-checkCollisionWithPlatforms() {
-}
-jump(){
-}
-moveright(){
-this.vx+=4;
-this.image.src = "botar imagem"
-}
-moveleft(){
-this.vx -= 4;
-this.image.src = "botar imagem"
-}
-draw(){
-this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-}
-}
-class platform{
-// construtor para a classe platform
-// x horizontal
-// y vertical
-constructor (x, y){
-this.context = canvas.getContext("2d");
-this.image = newImage();
-this.image.src = "foto do background";
-this.x = x;
-this.y = y;
-this.width = 100;
-this.height = 20;
-}
-}
+botao.addEventListener("click", comecar);
